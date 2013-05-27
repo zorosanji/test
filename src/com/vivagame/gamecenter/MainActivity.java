a5ebe4e6f61f16fc2e66ed6eae7aed7b01fc77a3
minsik. jung <androidzoro@gmail.com>
2013-05-27 16:30:01 +0900

    kkkkk

--- src/com/vivagame/gamecenter/MainActivity.java
+++ src/com/vivagame/gamecenter/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
 		int a;
+	String b;
         return true;
     }
 }
